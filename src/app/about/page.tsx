58a0765799d563a387bd2c49ac5a989cf4f91d90
murprakoso <prakoso.clan04@gmail.com>
2024-02-27 11:17:28 +0700

    routing metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+export const metadata = {
+  title: "About me",
+  description: "About me",
+};
+
 export default function About() {
   return (
     <div>
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,11 +1,27 @@
+import { Metadata } from "next";
+
 interface IParams {
   params: {
     productId: string;
   };
 }
 
+export const generateMetadata = async ({
+  params,
+}: IParams): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Product ${params.productId}`);
+    }, 1000);
+  });
+
+  return {
+    title: `Product ${title}`,
+    description: "ProductDetail page",
+  };
+};
+
 const ProductDetail = ({ params }: IParams) => {
-  console.log("ProductDetail: ", params);
   return (
     <div>
       <h1>ProductDetail : Product {params.productId}</h1>
